1fcd6baab615a243e659e2858581b3742651cd54
Rijo George <rijogeorge@macbook-pro-2.lan>
2021-11-11 18:25:59 -0800

    done

---
+++ script.js
@@ -0,0 +1,68 @@
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+  /* Write an ES2015 Version */
+
+  function createInstructor(firstName, lastName){
+    return {
+        firstName,
+        lastName
+    }
+  }
+
+var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+/* Write an ES2015 Version */
+
+var instructor = {
+      firstName: "Colt",
+      [favoriteNumber] : "That is my favorite!"
+    }
+
+    // var instructor = {
+    //     firstName: "Colt",
+    //     sayHi: function(){
+    //       return "Hi!";
+    //     },
+    //     sayBye: function(){
+    //       return this.firstName + " says bye!";
+    //     }
+    //   }
+
+      /* Write an ES2015 Version */
+
+      var instructor = {
+        firstName: "Colt",
+        sayHi(){
+          return "Hi!";
+        },
+        sayBye(){
+          return this.firstName + " says bye!";
+        }
+      }
+
+
+//       Write a function which generates an animal object. The function should accepts 3 arguments:
+
+// species: the species of animal (‘cat’, ‘dog’)
+// verb: a string used to name a function (‘bark’, ‘bleet’)
+// noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+
+      function createAnimal(species, verb, noise) {
+          return {
+            species,
+            [verb]() {
+                console.log(noise);
+            }
+          }
+      }
